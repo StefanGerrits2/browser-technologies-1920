a37ef424e0a7480bd4ea7b8fc937d1f435ef85c5
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-18 14:51:31 +0100

    setup simple node project

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+const publicPath = path.join(__dirname, './public/');
+
+app
+    .set('view engine', 'hbs')
+    .engine( 'hbs', hbs( {
+        extname: 'hbs',
+        defaultLayout: 'main',
+        partialsDir: __dirname + '/views/partials/'
+    }))
+    .use('/', express.static(publicPath))
+
+    // Get routes
+    .get('/', (req, res) => res.render('home.hbs'))
+
+    .listen(port, () => console.log(`Example app listening on port ${port}!`));
