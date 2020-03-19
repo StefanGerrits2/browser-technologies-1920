2d7d5f116664036dc44754c589674ff6dbfd42d2
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-19 20:52:56 +0100

    write input data to json file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+// var qs = require('querystring');
+const fs = require('file-system');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 const app = express();
 const port = 3000;
@@ -18,6 +22,41 @@ app
     // Get routes
     .get('/', (req, res) => res.render('home.hbs'))
     .get('/getCode', (req, res) => res.render('getCode.hbs'))
+
+    // Page 1
     .get('/questions-1', (req, res) => res.render('questions-1.hbs'))
+
+    // Page 2
+    // .get('/questions-2', (req, res) => res.render('questions-2.hbs'))
+    .post('/questions-2', urlencodedParser, (req, res) => {
+        const data = JSON.stringify(req.body, null, 2);
+        fs.writeFileSync('answers/data1.json', data);
+
+        console.log(data);
+
+        res.render('questions-2.hbs');
+    })
+
+    // Page 3
+    // .get('/questions-3', (req, res) => res.render('questions-3.hbs'))
+    .post('/questions-3', urlencodedParser, (req, res) => {
+        const data = JSON.stringify(req.body, null, 2);
+        fs.writeFile('answers/data1.json', data);
+
+        console.log(data);
+
+        res.render('questions-3.hbs');
+    })
+
+    // Finished
+    // .get('/finished', (req, res) => res.render('finished.hbs'))
+    .post('/finished', urlencodedParser, (req, res) => {
+        const data = JSON.stringify(req.body, null, 2);
+        fs.writeFile('answers/data1.json', data);
+
+        console.log(data);
+
+        res.render('finished.hbs');
+    })
 
     .listen(port, () => console.log(`Example app listening on port ${port}!`));
