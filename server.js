bb6953b5eafafb753e71e25f2e6acec4a7bce510
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-20 14:47:48 +0100

    fix routes, refactor user ix

--- server.js
+++ server.js
@@ -11,14 +11,10 @@ const port = process.env.PORT || 3000;
 
 const generator = require('generate-password');
 
-let user = '';
-
-const password = generator.generate({
+const user = generator.generate({
     length: 6,
     numbers: true
 });
-
-console.log(password);
 
 const app = express();
 const publicPath = path.join(__dirname, './public/');
@@ -34,43 +30,39 @@ app
 
     // Get routes
     .get('/', (req, res) => res.render('home.hbs'))
-    .get('/getCode', (req, res) => res.render('getCode.hbs', { password }))
+
+    // Code
+    .get('/getCode', (req, res) => res.render('getCode.hbs', { user }))
 
     // Page 1
-    // .get('/questions-1', (req, res) => res.render('questions-1.hbs'))
+    .get('/questions-1', (req, res) => res.render('questions-1.hbs'))
     .post('/questions-1', urlencodedParser, (req, res) => {
         const data = JSON.stringify(req.body, null, 2);
-        fs.writeFileSync(`answers/${password}.json`, data);
+        fs.writeFileSync(`answers/${user}.json`, data);
 
         console.log(data);
-
-        user = password;
 
         res.render('questions-1.hbs', { user } );
     })
 
     // Page 2
-    // .get('/questions-2', (req, res) => res.render('questions-2.hbs'))
+    .get('/questions-2', (req, res) => res.render('questions-2.hbs'))
     .post('/questions-2', urlencodedParser, (req, res) => {
         const data = JSON.stringify(req.body, null, 2);
-        fs.writeFileSync(`answers/${password}.json`, data);
+        fs.writeFileSync(`answers/${user}.json`, data);
 
         console.log(data);
-
-        user = password;
 
         res.render('questions-2.hbs', { user } );
     })
 
     // Page 3
-    // .get('/questions-3', (req, res) => res.render('questions-3.hbs'))
+    .get('/questions-3', (req, res) => res.render('questions-3.hbs'))
     .post('/questions-3', urlencodedParser, (req, res) => {
         const data = JSON.stringify(req.body, null, 2);
-        fs.writeFile(`answers/${password}.json`, data);
+        fs.writeFile(`answers/${user}.json`, data);
 
         console.log(data);
-
-        user = password;
 
         res.render('questions-3.hbs', { user });
     })
@@ -79,7 +71,7 @@ app
     // .get('/finished', (req, res) => res.render('finished.hbs'))
     .post('/finished', urlencodedParser, (req, res) => {
         const data = JSON.stringify(req.body, null, 2);
-        fs.writeFile(`answers/${password}.json`, data);
+        fs.writeFile(`answers/${user}.json`, data);
 
         console.log(data);
 
