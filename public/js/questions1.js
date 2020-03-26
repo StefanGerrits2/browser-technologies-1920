d5291bec9362bd7fefc657b955d870620b3cf0c5
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-26 11:50:54 +0100

    update styles

--- public/js/questions1.js
+++ public/js/questions1.js
@@ -1,7 +1,9 @@
-if (localStorage) {
-    console.log('localstorage is enabled');
-}
-
 if (window.localStorage) {
     console.log('window localstorage is enabled');
+
+    // var age = document.querySelector('#age');
+
+    // localStorage.setItem('age', JSON.stringify(age.value));
+
+    // age.value = JSON.parse(localStorage.getItem('age'));
 }
